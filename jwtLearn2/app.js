cc3745017bec74d77f22b0b216834dbb8cfe3c72
SreeraG <sreerag.nair@built.io>
2018-05-18 18:37:17 +0530

    Still working with Passport but bearer working

--- jwtLearn2/app.js
+++ jwtLearn2/app.js
@@ -8,7 +8,8 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var jwttoken = require('jsonwebtoken')
 var bodyparser = require('body-parser')
-var jwtStrategy = require('passport-jwt').ExtractJwt;
+var jwtStrategy = require('passport-jwt').Strategy
+var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 
 app.use(bodyparser.json())
@@ -17,7 +18,8 @@ app.use(passport.initialize())
 
 
 var user = {
-    email: 'sree',
+    // email: 'sree',
+    email : 'abc@test.com',
     password: 'abc'
 }
 
@@ -32,40 +34,54 @@ passport.deserializeUser(function (user, done) {
     done(err, user)
 })
 
+var opts = {}
+// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('bearer');
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = '4F7B5BF51329D01AB51430575797C66B';
 
+passport.use(new jwtStrategy(opts, function (jwtpload, done) {
+    
+    
+    console.log("JWT DATA : ", jwtpload)
+
+    if(user.email == jwtpload.emailId){
+        return done(null, user)
+        
+    }
+        else{
+
+        return done(null,false,{message : "Hello there"})
+        }
+    
+
+
+}))
 
 
 //THIS WORKS!!
-// passport.use('testUse', new LocalStrategy({
-//     // testUse is the name of the custom-strategy 
-//     usernameField: 'emailId',
-//     passwordField: 'password',
-//     //passReqToCallback: true // to pass back the entire request to the callback
-// }, function ( email, password, done) {
-//     // console.log(req);
-//     console.log(jwtStrategy.fromAuthHeaderAsBearerToken())
-//     console.log(email);
-//     console.log(password);
-//     // console.log(done);
-//     if (email == user.email) {
-//         return done(null, user)
-//     }
-//     else {
-//         return done(null, false, {message : 'HJGHBH'})
-//     }
-// }))
+passport.use('testUse', new LocalStrategy({
+    // testUse is the name of the custom-strategy 
+    usernameField: 'emailId',
+    passwordField: 'password',
+    //passReqToCallback: true // to pass back the entire request to the callback
+}, function (email, password, done) {
+    // console.log(req);
+    console.log(jwtStrategy.fromAuthHeaderAsBearerToken())
+    console.log(email);
+    console.log(password);
+    // console.log(done);
+    if (email == user.email) {
+        return done(null, user)
+    }
+    else {
+        return done(null, false, { message: 'HJGHBH' })
+    }
+}))
 
-function s() {
-    console.log("SUCCESS")
-
-}
-function f() {
-    console.log("FAILURE!")
-}
 
 app.post('/',
     // console.log("HERE ME OUT!")
-    passport.authenticate('testUse'), function (req, res) {
+    passport.authenticate('jwt'), function (req, res) {
         console.log("REQUEST.BODY : ", req.headers)
     }
 )
